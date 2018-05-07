db820f362ba6f729a3164df1ef3539eb07c9acf5
gxfeng06 <308806988@qq.com>
2018-05-07 15:37:59 +0800

    first commit

---
+++ kScrollview/kScrollview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kScrollview
+//
+//  Created by superMan on 2018/5/7.
+//  Copyright © 2018年 gxfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
